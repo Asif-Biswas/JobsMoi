372090908b8f179f10539edf71e6fc86f1f26d17
Asif Biswas <asifbiswas85@gmail.com>
2023-05-02 13:55:29 +0600

    comment interview admin

--- interview/admin.py
+++ interview/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Interview, InterviewComment
 
 # Register your models here.
+admin.site.register(Interview)
+admin.site.register(InterviewComment)
