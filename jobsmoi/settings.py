167493918377b969cddc8077c71876d34fb24319
Asif Biswas <asifbiswas85@gmail.com>
2023-05-02 13:32:27 +0600

    whitenoise

--- jobsmoi/settings.py
+++ jobsmoi/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,6 +156,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'homePage/static'),
 )
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
